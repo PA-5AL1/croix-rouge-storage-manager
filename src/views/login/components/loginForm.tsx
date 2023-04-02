26fcc610d6674492f6b2868fe2c13655593461e4
Hang <hanassser@gmail.com>
2023-04-02 23:19:09 +0200

    feat: structure and login

--- src/views/login/components/loginForm.tsx
+++ src/views/login/components/loginForm.tsx
@@ -1,38 +1,36 @@
 import React from 'react';
-import { Button, Checkbox, Form, Input } from 'antd';
+import { Button, Form, Input } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
-function LoginForm(){
- return(
-	 <Form
-		 name="basic"
-		 style={{ maxWidth: 600 }}
-		 initialValues={{ remember: true }}
-		 autoComplete="off"
-	 >
-		 <Form.Item
+function LoginForm() {
+	return (
+		<Form
+			name="basic"
+			style={{ maxWidth: 600 }}
+			initialValues={{ remember: true }}
+			autoComplete="off"
+		>
+			<Form.Item
+				name="username"
+				rules={[{ required: true, message: 'veuillez entrer e-mail' }]}
+			>
+				<Input prefix={<UserOutlined/>} placeholder="veuillez entrer e-mail"  />
+			</Form.Item>
 
-			 name="username"
-			 rules={[{ required: true, message: 'Please input your username!' }]}
-		 >
-			 <Input />
-		 </Form.Item>
+			<Form.Item
+				name="password"
+				rules={[{ required: true, message: 'veuillez entrer le mot de passe' }]}
+			>
+				<Input.Password prefix={<LockOutlined/>} placeholder="veuillez entrer le mot de passe" />
+			</Form.Item>
 
-		 <Form.Item
-
-			 name="password"
-			 rules={[{ required: true, message: 'Please input your password!' }]}
-		 >
-			 <Input.Password />
-		 </Form.Item>
-
-
-		 <Form.Item >
-			 <Button type="primary" htmlType="submit" block>
-				 Login
-			 </Button>
-		 </Form.Item>
-	 </Form>
- )
+			<Form.Item>
+				<Button type="primary" htmlType="submit" block>
+					Login
+				</Button>
+			</Form.Item>
+		</Form>
+	);
 }
 
-export default LoginForm
+export default LoginForm;
